60ec0b17e85c489f9547c8e700aed393be38f77e
Lukasz Gandecki <lgandecki@css.edu>
2017-05-12 21:01:53 +0200

    rollup code size visualization

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,18 +1,22 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import Visualizer from 'rollup-plugin-visualizer';
 
 export default {
   entry: 'src/modify.js',
   format: 'cjs',
   dest: 'dist/bundle.js',
-  plugins: [resolve({
-    // pass custom options to the resolve plugin
-    customResolveOptions: {
-      moduleDirectory: 'node_modules'
-    }
-  }),
-  babel({
-    exclude: 'node_modules/**'
-  })],
+  plugins: [
+    resolve({
+      // pass custom options to the resolve plugin
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    Visualizer()
+  ],
   external: ['underscore']
 };
